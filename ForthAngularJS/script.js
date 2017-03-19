d6837caa8ed0c663b0a12f00e25b2db55ee62f5c
Kunal Londhe <londhe.kunal@gmail.com>
2017-03-19 19:19:03 +0530

    ForthAngularJS
    ForthAngularJS

---
+++ ForthAngularJS/script.js
@@ -0,0 +1,32 @@
+(function() {
+
+  var app = angular.module("firstmodule", []);
+
+  var MainController = function($scope, $http) {
+
+    var OnComplete = function(response) {
+      $scope.user = response.data;
+      
+      var repourl = $scope.user.repos_url;
+      $http.get(repourl).then(OnRepo, OnError);
+    };
+
+    var OnRepo = function(response){
+      $scope.repoData = response.data;
+    };
+
+    var OnError = function(reason) {
+      $scope.error = "Could not featch data";
+    };
+
+    $scope.search = function(username) {
+      var promise = $http.get("https://api.github.com/users/" + username);
+      promise.then(OnComplete, OnError);
+    };
+
+    $scope.username = "angular";
+  };
+
+  app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
